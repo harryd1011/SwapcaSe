5cedf01cc2fb716083e921982e55c61528d8a0d2
harryd1011 <98024123+harryd1011@users.noreply.github.com>
2022-02-20 23:08:06 +0530

    Create Lapindrome.py

---
+++ Lapindrome.py
@@ -0,0 +1,15 @@
+Cases = int(input())
+while Cases:
+    Cases -= 1
+    string = input()        # input is already in string format so no need to type-cast
+    halve = len(string) // 2
+    if len(string) % 2 == 0:
+        if sorted(string[:halve]) == sorted(string[halve:]):
+            print('YES')
+        else:
+            print('NO')
+    else:
+        if sorted(string[:halve]) == sorted(string[halve + 1:]):
+            print('YES')
+        else:
+            print('NO')
